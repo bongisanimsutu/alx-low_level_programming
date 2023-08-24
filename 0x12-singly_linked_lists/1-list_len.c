b532e416e4043e539afaa0228e7f2d73dc69bd7c
bongisanimsutu <bongisanimsutu@gmail.com>
2023-08-24 12:47:50 -0700

    adding

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,19 +2,17 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
- * @h: pointer to the list_t list
- *
- * Return: number of elements in h
+ * list - returns the number of elements in a linked list
+ *m: pointer to the list_t list
  */
-size_t list_len(const list_t *h)
+size_t list(const list_t *m)
 {
 	size_t n = 0;
 
-	while (h)
+	while (m)
 	{
 		n++;
-		h = h->next;
+		m = m->next;
 	}
 	return (n);
 }
