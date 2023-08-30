77d1e90628f2664a94c1e24ceb810ad20af2e4db
bongisanimsutu <bongisanimsutu@gmail.com>
2023-08-30 13:26:38 -0700

    addin

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,17 +1,22 @@
 #include "lists.h"
 
 /**
- * free_listint - releases memory of a linked list
- * @head: listint_t list to be deallocated
+ * free_listint2 - deallocates a linked list
+ * @head: pointer to the listint_t list to be freed
  */
-void free_listint(listint_t *head)
+void free_listint2(listint_t **head)
 {
     listint_t *current;
 
-    while (head)
+    if (head == NULL)
+        return;
+
+    while (*head)
     {
-        current = head->next;
-        free(head);
-        head = current;
+        current = (*head)->next;
+        free(*head);
+        *head = current;
     }
+
+    *head = NULL;
 }
